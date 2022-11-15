900169a490ae0d07986a0f6447aeb47f89a9c1c6
Alexander Orlov <bredanych@gmail.com>
2022-11-15 16:41:48 +0300

    Main.kt Changed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val amount = 1000000
+    val taxRate = 0.0075
+    val taxValue = if (amount * taxRate <= 35) 35 else amount * taxRate
+    println(taxValue)
 }
