64fbaad8da37364804d021e967e2da0d1e182017
Alexander Orlov <bredanych@gmail.com>
2022-11-16 16:50:18 +0300

    Main.kt Tasks Solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,29 @@
 fun main() {
+    //Задача номер 1
     val amount = 1000000
     val taxRate = 0.0075
-    val taxValue = if (amount * taxRate <= 35) 35 else amount * taxRate
-    println(taxValue)
+    val minTax = 35
+    val taxValue = if (amount * taxRate <= minTax) minTax else amount * taxRate
+    println(taxValue.toString() + " - Ответ задачи №1")
+
+    //Задача номер 2
+    val likes = 221
+    val people = if (likes % 100 != 11 && likes % 10 == 1) "человеку" else "людям"
+    println("Понравилось " + likes + " " + people + " - Ответ задачи номер 2")
+
+    //Задача номер 3
+    val regUser = true
+    val regUserDiscount = 1
+    val priceValue = 1500
+    val fixDiscount = 100
+    val discount = 5
+    val dPrice = if (priceValue < 1001) {
+        priceValue
+    } else if (priceValue >= 1001 && priceValue < 10001) {
+        (priceValue - fixDiscount)
+    } else if (priceValue >= 10001) {
+        priceValue - (priceValue * discount/100)
+    } else 0
+    val resultPrice = if (regUser) (dPrice - (dPrice * regUserDiscount/100)) else dPrice
+    println("Конечная цена : " + resultPrice + " - Ответ на задачу номер 3")
 }
